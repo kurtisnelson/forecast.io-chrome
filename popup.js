f270cd130b75c8fb81923dac371c47e1fa59153c
Kurt Nelson <kurtisnelson@gmail.com>
2013-04-15 20:47:30 -0400

    different skycons

--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
 var skycons = new Skycons();
+var iconName = localStorage.currentIcon.toUpperCase().replace(/-/g, '_');
+skycons.set("icon", Skycons[iconName]);
 if(localStorage.currentIcon == "partly-cloudy-night") {
-        skycons.set("icon", Skycons.PARTLY_CLOUDY_NIGHT);
+        skycons.set("icon", Skycons["PARTLY_CLOUDY_NIGHT"]);
 }
 skycons.play();
